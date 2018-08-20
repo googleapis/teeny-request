ae939291759f3ace346469efaba98b4fdb24b14e
Franziska Hinkelmann <franzih@chromium.org>
2018-08-20 19:45:59 -0400

    Add default behavior

--- src/index.ts
+++ src/index.ts
@@ -6,49 +6,56 @@ import * as f from 'node-fetch';
 
 
 function requestToFetchOptions(reqOpts: r.OptionsWithUri) {
-    const options: f.RequestInit = {
-        ...reqOpts.headers && { headers: reqOpts.headers },
-        ...reqOpts.method && { method: reqOpts.method },
-        ...reqOpts.json && { body: JSON.stringify(reqOpts.json) },
-    };
+  const options: f.RequestInit = {
+    ...reqOpts.headers && { headers: reqOpts.headers },
+    ...reqOpts.method && { method: reqOpts.method },
+    ...reqOpts.json && { body: JSON.stringify(reqOpts.json) },
+  };
 
-    let uri: string = reqOpts.uri as string;
-    if (reqOpts.useQuerystring === true) {
-        const qs = require('querystring');
-        const params = qs.stringify(reqOpts.qs);
-        uri = uri + '?' + params;
-    }
+  let uri: string = reqOpts.uri as string;
+  if (reqOpts.useQuerystring === true) {
+    const qs = require('querystring');
+    const params = qs.stringify(reqOpts.qs);
+    uri = uri + '?' + params;
+  }
 
-    return [uri, options];
+  return [uri, options];
 }
 
 function fetchToRequestResponse(res: f.Response) {
-    const response: r.Response = {
-        statusCode: res.status,
-        statusMessage: res.statusText,
-    } as r.Response;
-    return response;
+  const response: r.Response = {
+    statusCode: res.status,
+    statusMessage: res.statusText,
+  } as r.Response;
+  return response;
 }
 
 
 function teenyRequest(reqOpts: r.OptionsWithUri, callback: any) {
-    const [uri, options] = requestToFetchOptions(reqOpts);
+  reqOpts.timeout = reqOpts.timeout || 60000;
+  reqOpts.gzip = reqOpts.gzip || true;
+  reqOpts.forever = reqOpts.forever || true;
+  if (!reqOpts.pool) {
+    reqOpts.pool = {};
+  }
+  reqOpts.pool.maxSockets = reqOpts.pool.maxSockets || Infinity;
 
-    fetch(uri as string, options as f.RequestInit)
-        .then((res: f.Response) => {
-            if (!res.ok) {
-                callback(new Error(`${res.status}: ${res.statusText}`));
-                return;
-            }
-            res.json().then(json => {
-                let response = fetchToRequestResponse(res);
-                response.body = json;
-                callback(null, response, json);
-            }).catch((err) => {
-                callback(err);
-            });
-        });
+  const [uri, options] = requestToFetchOptions(reqOpts);
+
+  fetch(uri as string, options as f.RequestInit)
+    .then((res: f.Response) => {
+      if (!res.ok) {
+        callback(new Error(`${res.status}: ${res.statusText}`));
+        return;
+      }
+      res.json().then(json => {
+        let response = fetchToRequestResponse(res);
+        response.body = json;
+        callback(null, response, json);
+      }).catch((err) => {
+        callback(err);
+      });
+    });
 }
-
 
 export { teenyRequest };
