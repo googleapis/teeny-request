3781762db3d118319f8b011e635dcb731d7956b8
Franziska Hinkelmann <franzih@chromium.org>
2018-08-24 21:27:47 -0400

    Use gts
    Use [Google TypeScript style](https://www.npmjs.com/package/gts).
    
    Fixes: https://github.com/fhinkel/teeny-request/issues/2

--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,112 @@
 'use strict';
 
 import * as r from 'request';
-import fetch from 'node-fetch';
-import * as f from 'node-fetch';
+import fetch, * as f from 'node-fetch';
 // tslint:disable-next-line variable-name
 const HttpsProxyAgent = require('https-proxy-agent');
 
-function requestToFetchOptions(reqOpts: r.OptionsWithUri) {
 
-  const options: f.RequestInit = {
-    ...reqOpts.headers && { headers: reqOpts.headers },
-    ...reqOpts.method && { method: reqOpts.method },
-    ...reqOpts.json && { body: JSON.stringify(reqOpts.body) },
-    ...reqOpts.timeout && { timeout: reqOpts.timeout },
-    ...reqOpts.gzip && { compress: reqOpts.gzip },
-
-  };
-
-  let uri: string = reqOpts.uri as string;
-  if (reqOpts.useQuerystring === true) {
-    const qs = require('querystring');
-    const params = qs.stringify(reqOpts.qs);
-    uri = uri + '?' + params;
-  }
-
-  if (reqOpts.proxy || process.env.HTTP_PROXY || process.env.HTTPS_PROXY) {
-    const proxy = (process.env.HTTP_PROXY || process.env.HTTPS_PROXY)!;
-    options.agent = new HttpsProxyAgent(proxy);
-  }
-
-  return [uri, options];
+interface RequestToFetchOptions {
+  (reqOpts: r.OptionsWithUri): [string, f.RequestInit];
 }
 
-function fetchToRequestResponse(res: f.Response) {
+const requestToFetchOptions: RequestToFetchOptions =
+    (reqOpts: r.OptionsWithUri) => {
+      const options: f.RequestInit = {
+        ...reqOpts.headers && {headers: reqOpts.headers},
+        ...reqOpts.method && {method: reqOpts.method},
+        ...reqOpts.json && {body: JSON.stringify(reqOpts.body)},
+        ...reqOpts.timeout && {timeout: reqOpts.timeout},
+        ...reqOpts.gzip && {compress: reqOpts.gzip},
+
+      };
+
+      let uri: string = reqOpts.uri as string;
+      if (reqOpts.useQuerystring === true) {
+        const qs = require('querystring');
+        const params = qs.stringify(reqOpts.qs);
+        uri = uri + '?' + params;
+      }
+
+      if (reqOpts.proxy || process.env.HTTP_PROXY || process.env.HTTPS_PROXY) {
+        const proxy = (process.env.HTTP_PROXY || process.env.HTTPS_PROXY)!;
+        options.agent = new HttpsProxyAgent(proxy);
+      }
+
+      return [uri, options];
+    };
+
+interface FetchToRequestResponse {
+  (res: f.Response): r.Response;
+}
+const fetchToRequestResponse = (res: f.Response) => {
   const response: r.Response = {
     statusCode: res.status,
     statusMessage: res.statusText,
   } as r.Response;
   return response;
+};
+
+
+interface Callback {
+  (err: Error|null, response?: r.Response, body?: {}|string): void;
 }
 
-function teenyRequest(reqOpts: r.OptionsWithUri, callback?: any) {
-  const [uri, options] = requestToFetchOptions(reqOpts);
-  fetch(uri as string, options as f.RequestInit)
-    .then((res: f.Response) => {
-      if (!res.ok) {
-        callback(new Error(`${res.status}: ${res.statusText}`));
-        return;
-      }
-      let header = res.headers.get('content-type');
-      if (header === 'application/json' || header === 'application/json; charset=utf-8') {
-        let response = fetchToRequestResponse(res);
-        res.json().then(json => {
-          response.body = json;
-          callback(null, response, json);
-        }).catch(err => {
-          callback(err);
-        });
-        return;
-      }
-
-      res.text().then(text => {
-        let response = fetchToRequestResponse(res);
-        response.body = text;
-        callback(null, response, text);
-
-      }).catch(err => {
-        callback(err);
-      })
-    }).catch(err => {
-      callback(err);
-    });
+// Mimics `request`. Can be used as `request(options, callback)`
+// or `request.defaults(opts)(options, callback)`
+interface TeenyRequest {
+  (reqOpts: r.OptionsWithUri, callback: Callback): void;
+  defaults:
+      ((options: r.OptionsWithUri) =>
+           ((reqOpts: r.OptionsWithUri, callback: Callback) => void));
 }
 
-namespace teenyRequest {
-  export function defaults(defaults: r.OptionsWithUri) {
-    return function (reqOpts: r.OptionsWithUri, callback: any) {
-      teenyRequest({ ...defaults, ...reqOpts }, callback);
+const teenyRequest =
+    ((reqOpts: r.OptionsWithUri, callback: Callback) => {
+      const [uri, options] = requestToFetchOptions(reqOpts);
+      fetch(uri as string, options as f.RequestInit)
+          .then((res: f.Response) => {
+            if (!res.ok) {
+              callback(new Error(`${res.status}: ${res.statusText}`));
+              return;
+            }
+            const header = res.headers.get('content-type');
+            if (header === 'application/json' ||
+                header === 'application/json; charset=utf-8') {
+              const response = fetchToRequestResponse(res);
+              res.json()
+                  .then(json => {
+                    response.body = json;
+                    callback(null, response, json);
+                  })
+                  .catch((err: Error) => {
+                    callback(err);
+                  });
+              return;
+            }
 
-    };
-  }
-}
+            res.text()
+                .then(text => {
+                  const response = fetchToRequestResponse(res);
+                  response.body = text;
+                  callback(null, response, text);
+                })
+                .catch(err => {
+                  callback(err);
+                });
+          })
+          .catch((err: Error) => {
+            callback(err);
+          });
+    }) as TeenyRequest;
 
-export { teenyRequest };
+teenyRequest.defaults = (defaults: r.OptionsWithUri) => {
+  return (reqOpts: r.OptionsWithUri,
+          callback:
+              (err: Error|null, response?: r.Response, body?: {}|string) =>
+                  void) => {
+    teenyRequest({...defaults, ...reqOpts}, callback);
+  };
+};
+
+export {teenyRequest};
--- test/index.ts
+++ test/index.ts
@@ -1,29 +1,35 @@
 
 'use strict';
 
-import { teenyRequest } from '../src';
 import * as assert from 'assert';
+import * as r from 'request';
+
+import {teenyRequest} from '../src';
 
 describe('teeny', () => {
   it('should get JSON', (done) => {
-    teenyRequest({ uri: 'https://jsonplaceholder.typicode.com/todos/1' }, function (error: any, response: any, body: any) {
-      assert.ifError(error);
-      assert.strictEqual(response.statusCode, 200);
-      console.log(body.ip);
-      assert.notEqual(body.userId, null);
+    teenyRequest(
+        {uri: 'https://jsonplaceholder.typicode.com/todos/1'},
+        // tslint:disable-next-line:no-any
+        (error: Error|null, response?: r.Response, body?: any) => {
+          assert.ifError(error);
+          assert.strictEqual(response!.statusCode, 200);
+          assert.notEqual(body!.userId, null);
 
-      done();
-    });
-  }),
-    it('should set defaults', (done) => {
-      let defaultRequest = teenyRequest.defaults({ timeout: 60000 } as any);
-      defaultRequest({ uri: 'https://jsonplaceholder.typicode.com/todos/1' }, function (error: any, response: any, body: any) {
-        assert.ifError(error);
-        assert.strictEqual(response.statusCode, 200);
-        console.log(body.ip);
-        assert.notEqual(body.userId, null);
+          done();
+        });
+  }), it('should set defaults', (done) => {
+    const defaultRequest =
+        teenyRequest.defaults({timeout: 60000} as r.OptionsWithUri);
+    defaultRequest(
+        {uri: 'https://jsonplaceholder.typicode.com/todos/1'},
+        // tslint:disable-next-line:no-any
+        (error: Error|null, response?: r.Response, body?: any) => {
+          assert.ifError(error);
+          assert.strictEqual(response!.statusCode, 200);
+          assert.notEqual(body!.userId, null);
 
-        done();
-      });
-    })
+          done();
+        });
+  });
 });
