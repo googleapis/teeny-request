991276ebeea6ed7da9e97b7efbb0d7040a5f21c3
Franziska Hinkelmann <franzih@chromium.org>
2018-09-18 09:59:07 -0400

    Looks like upoad of multistream with zip works

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 
 import * as r from 'request';
 import fetch, * as f from 'node-fetch';
-import {PassThrough} from 'stream';
+import { PassThrough } from 'stream';
 // tslint:disable-next-line variable-name
 const HttpsProxyAgent = require('https-proxy-agent');
 
@@ -13,43 +13,43 @@ interface RequestToFetchOptions {
 }
 
 const requestToFetchOptions: RequestToFetchOptions =
-    (reqOpts: r.OptionsWithUri) => {
-      const options: f.RequestInit = {
-        method: reqOpts.method || 'GET',
-        ...reqOpts.timeout && {timeout: reqOpts.timeout},
-        ...reqOpts.gzip && {compress: reqOpts.gzip},
+  (reqOpts: r.OptionsWithUri) => {
+    const options: f.RequestInit = {
+      method: reqOpts.method || 'GET',
+      ...reqOpts.timeout && { timeout: reqOpts.timeout },
+      ...reqOpts.gzip && { compress: reqOpts.gzip },
 
-      };
-
-      if (typeof reqOpts.json === 'object') {
-        // Add Content-type: application/json header
-        if (!reqOpts.headers) {
-          reqOpts.headers = {};
-        }
-        reqOpts.headers['Content-Type'] = 'application/json';
-
-        // Set body to JSON representation of value
-        options.body = JSON.stringify(reqOpts.json);
-      } else {
-        options.body = JSON.stringify(reqOpts.body);
-      }
-
-      options.headers = reqOpts.headers;
-
-      let uri: string = reqOpts.uri as string;
-      if (reqOpts.useQuerystring === true || typeof reqOpts.qs === 'object') {
-        const qs = require('querystring');
-        const params = qs.stringify(reqOpts.qs);
-        uri = uri + '?' + params;
-      }
-
-      if (reqOpts.proxy || process.env.HTTP_PROXY || process.env.HTTPS_PROXY) {
-        const proxy = (process.env.HTTP_PROXY || process.env.HTTPS_PROXY)!;
-        options.agent = new HttpsProxyAgent(proxy);
-      }
-
-      return [uri, options];
     };
+
+    if (typeof reqOpts.json === 'object') {
+      // Add Content-type: application/json header
+      if (!reqOpts.headers) {
+        reqOpts.headers = {};
+      }
+      reqOpts.headers['Content-Type'] = 'application/json';
+
+      // Set body to JSON representation of value
+      options.body = JSON.stringify(reqOpts.json);
+    } else {
+      options.body = JSON.stringify(reqOpts.body);
+    }
+
+    options.headers = reqOpts.headers;
+
+    let uri: string = reqOpts.uri as string;
+    if (reqOpts.useQuerystring === true || typeof reqOpts.qs === 'object') {
+      const qs = require('querystring');
+      const params = qs.stringify(reqOpts.qs);
+      uri = uri + '?' + params;
+    }
+
+    if (reqOpts.proxy || process.env.HTTP_PROXY || process.env.HTTPS_PROXY) {
+      const proxy = (process.env.HTTP_PROXY || process.env.HTTPS_PROXY)!;
+      options.agent = new HttpsProxyAgent(proxy);
+    }
+
+    return [uri, options];
+  };
 
 interface FetchToRequestResponse {
   (res: f.Response): r.Response;
@@ -64,7 +64,7 @@ const fetchToRequestResponse = (res: f.Response) => {
 
 
 interface Callback {
-  (err: Error|null, response?: r.Response, body?: {}|string): void;
+  (err: Error | null, response?: r.Response, body?: {} | string): void;
 }
 
 // Mimics `request`. Can be used as `request(options, callback)`
@@ -72,153 +72,172 @@ interface Callback {
 interface TeenyRequest {
   (reqOpts: r.OptionsWithUri, callback: Callback): void;
   defaults:
-      ((options: r.OptionsWithUri) =>
-           ((reqOpts: r.OptionsWithUri, callback: Callback) => void));
+  ((options: r.OptionsWithUri) =>
+    ((reqOpts: r.OptionsWithUri, callback: Callback) => void));
 }
 
 const teenyRequest =
-    ((reqOpts: r.OptionsWithUri, callback?: Callback) => {
-      const [uri, options] = requestToFetchOptions(reqOpts);
+  ((reqOpts: r.OptionsWithUri, callback?: Callback) => {
+    const [uri, options] = requestToFetchOptions(reqOpts);
 
-      // tslint:disable-next-line:no-any
-      const multipart: any[] = (reqOpts.multipart as any[]);
-      if (reqOpts.multipart && multipart.length === 2) {
-        // tslint:disable-next-line:no-any
-        (options.headers as any)['Content-Type'] =
-            JSON.parse((multipart[0] as {body: string}).body).contentType;
+    // tslint:disable-next-line:no-any
+    const multipart: { 'Content-Type': string, body: any }[] = (reqOpts.multipart as any[]);
+    if (reqOpts.multipart && multipart.length === 2) {
 
-        // tslint:disable-next-line:no-any
-        if ((options.headers as any)['Content-Type'] === undefined) {
-          // tslint:disable-next-line:no-any
-          (options.headers as any)['Content-Type'] =
-              (multipart[1] as {'Content-Type': string})['Content-Type'];
+      const boundary = 'someRandomBoundaryString';
+      const finale = `--${boundary}--`;
+      const multiHeaders: { 'Content-Type': string } = {
+        'Content-Type': `multipart/related; boundary=${boundary}`
+      };
+      const mediaStream: PassThrough = new PassThrough();
+
+      for (const part of multipart) {
+        const preamble =
+          `--${boundary}\r\nContent-Type: ${part['Content-Type']}\r\n\r\n`;
+        mediaStream.push(preamble);
+        if (typeof part.body === 'string') {
+          mediaStream.push(part.body);
+          mediaStream.push('\r\n');
+        } else {
+
+          // var zlib = require('zlib');
+          // var gzip = zlib.createGzip();
+          // part.body.pipe(gzip).pipe(mediaStream, { end: false });
+
+          part.body.pipe(mediaStream, { end: false });
+          part.body.on('end', () => {
+            mediaStream.push('\r\n');
+            mediaStream.push(finale);
+            mediaStream.push(null);
+          });
         }
+      }
+      (options.headers as any)['Content-Type'] = multiHeaders['Content-Type'];
+      options.compress = false;
+      options.body = mediaStream;
 
-        const stream = multipart[1].body;  // Transform
-        options.body = stream;
+      // Multipart
+      fetch(uri as string, options as f.RequestInit)
+        .then((res: f.Response) => {
+          const header = res.headers.get('content-type');
+          if (header === 'application/json' ||
+            header === 'application/json; charset=utf-8') {
+            const response = fetchToRequestResponse(res);
+            if (response.statusCode === 204) {
+              // Probably a DELETE
+              callback!(null, response, response);
+              return;
+            }
+            res.json()
+              .then(json => {
+                response.body = json;
+                callback!(null, response, json);
+              })
+              .catch((err: Error) => {
+                callback!(err);
+              });
+            return;
+          }
 
-        // Multipart
-        fetch(uri as string, options as f.RequestInit)
-            .then((res: f.Response) => {
-              const header = res.headers.get('content-type');
-              if (header === 'application/json' ||
-                  header === 'application/json; charset=utf-8') {
-                const response = fetchToRequestResponse(res);
-                if (response.statusCode === 204) {
-                  // Probably a DELETE
-                  callback!(null, response, response);
-                  return;
-                }
-                res.json()
-                    .then(json => {
-                      response.body = json;
-                      callback!(null, response, json);
-                    })
-                    .catch((err: Error) => {
-                      callback!(err);
-                    });
-                return;
-              }
+          res.text()
+            .then(text => {
+              const response = fetchToRequestResponse(res);
+              response.body = text;
+              callback!(null, response, text);
+            })
+            .catch(err => {
+              callback!(err);
+            });
+        })
+        .catch((err: Error) => {
+          callback!(err);
+        });
+      return;
+    }
 
-              res.text()
-                  .then(text => {
-                    const response = fetchToRequestResponse(res);
-                    response.body = text;
-                    callback!(null, response, text);
-                  })
-                  .catch(err => {
-                    callback!(err);
-                  });
+    if (callback === undefined) {  // Stream mode
+      const requestStream: PassThrough = new PassThrough();
+      fetch(uri as string, options as f.RequestInit)
+        .then((res: f.Response) => {
+          if (!res.ok) {
+            // tslint:disable-next-line:no-any
+            const error: any = new Error(res.statusText);
+            error.code = res.status;
+            requestStream.emit('error', error);
+            return;
+          }
+
+          const encoding = res.headers.get('content-type');
+          res.body.on('error', err => {
+            console.log('whoa there was an error, passing it on' + err);
+            requestStream.emit('error', err);
+          });
+
+          // tslint:disable-next-line:no-any
+          (res.body as any).toJSON = () => {
+            const headers:
+              { 'content-encoding': string } = { 'content-encoding': 'gzip' };
+            return { headers };
+          };
+
+          requestStream.emit('response', res.body);
+        })
+        .catch((err: Error) => {
+          console.log('such a nice error:' + err);
+          requestStream.emit('error', err);
+        });
+
+      // fetch doesn't supply the raw HTTP stream, instead it
+      // returns a PassThrough piped from the HTTP response
+      // stream
+      return requestStream;
+    }
+    // regular fetch
+    fetch(uri as string, options as f.RequestInit)
+      .then((res: f.Response) => {
+        const header = res.headers.get('content-type');
+        if (header === 'application/json' ||
+          header === 'application/json; charset=utf-8') {
+          const response = fetchToRequestResponse(res);
+          if (response.statusCode === 204) {
+            // Probably a DELETE
+            callback!(null, response, response);
+            return;
+          }
+          res.json()
+            .then(json => {
+              response.body = json;
+              callback!(null, response, json);
             })
             .catch((err: Error) => {
               callback!(err);
             });
-        return;
-      }
+          return;
+        }
 
-      if (callback === undefined) {  // Stream mode
-        const requestStream: PassThrough = new PassThrough();
-        fetch(uri as string, options as f.RequestInit)
-            .then((res: f.Response) => {
-              if (!res.ok) {
-                // tslint:disable-next-line:no-any
-                const error: any = new Error(res.statusText);
-                error.code = res.status;
-                requestStream.emit('error', error);
-                return;
-              }
-
-              const encoding = res.headers.get('content-type');
-              res.body.on('error', err => {
-                console.log('whoa there was an error, passing it on' + err);
-                requestStream.emit('error', err);
-              });
-
-              // tslint:disable-next-line:no-any
-              (res.body as any).toJSON = () => {
-                const headers:
-                    {'content-encoding': string} = {'content-encoding': 'gzip'};
-                return {headers};
-              };
-
-              requestStream.emit('response', res.body);
-            })
-            .catch((err: Error) => {
-              console.log('such a nice error:' + err);
-              requestStream.emit('error', err);
-            });
-
-        // fetch doesn't supply the raw HTTP stream, instead it
-        // returns a PassThrough piped from the HTTP response
-        // stream
-        return requestStream;
-      }
-      // regular fetch
-      fetch(uri as string, options as f.RequestInit)
-          .then((res: f.Response) => {
-            const header = res.headers.get('content-type');
-            if (header === 'application/json' ||
-                header === 'application/json; charset=utf-8') {
-              const response = fetchToRequestResponse(res);
-              if (response.statusCode === 204) {
-                // Probably a DELETE
-                callback!(null, response, response);
-                return;
-              }
-              res.json()
-                  .then(json => {
-                    response.body = json;
-                    callback!(null, response, json);
-                  })
-                  .catch((err: Error) => {
-                    callback!(err);
-                  });
-              return;
-            }
-
-            res.text()
-                .then(text => {
-                  const response = fetchToRequestResponse(res);
-                  response.body = text;
-                  callback!(null, response, text);
-                })
-                .catch(err => {
-                  callback!(err);
-                });
+        res.text()
+          .then(text => {
+            const response = fetchToRequestResponse(res);
+            response.body = text;
+            callback!(null, response, text);
           })
-          .catch((err: Error) => {
+          .catch(err => {
             callback!(err);
           });
-      return;
-    }) as TeenyRequest;
+      })
+      .catch((err: Error) => {
+        callback!(err);
+      });
+    return;
+  }) as TeenyRequest;
 
 teenyRequest.defaults = (defaults: r.OptionsWithUri) => {
   return (reqOpts: r.OptionsWithUri,
-          callback:
-              (err: Error|null, response?: r.Response, body?: {}|string) =>
-                  void) => {
-    return teenyRequest({...defaults, ...reqOpts}, callback);
+    callback:
+      (err: Error | null, response?: r.Response, body?: {} | string) =>
+        void) => {
+    return teenyRequest({ ...defaults, ...reqOpts }, callback);
   };
 };
 
-export {teenyRequest};
+export { teenyRequest };
