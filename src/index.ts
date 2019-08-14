9f07e98a043cdc6dec8bfdf17fac8bbbf4e004f5
Benjamin E. Coe <bencoe@google.com>
2019-08-14 14:17:09 -0700

    fix(types): make types less strict for method (#76)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2018 Google LLC. All Rights Reserved.
+ * Copyright 2018 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,17 +23,7 @@ import * as uuid from 'uuid';
 const streamEvents = require('stream-events');
 
 export interface CoreOptions {
-  method?:
-    | 'GET'
-    | 'POST'
-    | 'PUT'
-    | 'HEAD'
-    | 'OPTIONS'
-    | 'PATCH'
-    | 'DELETE'
-    | 'CONNECT'
-    | 'OPTIONS'
-    | 'TRACE';
+  method?: string;
   timeout?: number;
   gzip?: boolean;
   // tslint:disable-next-line no-any
